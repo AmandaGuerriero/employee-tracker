c0195da29d4cd5fcdfb26bf5f734430f0eadd05e
Amanda Guerriero <ag.codingclass@gmail.com>
2020-10-23 13:44:27 -0700

    initial setup, connected, and prompted a question

--- db/database.js
+++ db/database.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2');
+const util = require('util');
 
 // Connnect to database
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'NBfury009!Rose',
+    password: 'password',
     database: 'employee_db'
 });
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -3,12 +3,32 @@ const { prompt } = require('inquirer');
 const cTable = require('console.table');
 const connection = require('./db/database');
 
-const PORT = process.env.PORT || 3001;
+init();
+
+function init() {
+    console.log ("Hello, Welcome to Employee Tracker!");   
+    loadMainPrompts();
+}
+
+// Load Questions
+async function loadMainPrompts() {
+    const { choice } = await prompt([
+       {
+           type: 'list',
+           name: 'choice',
+           message: 'What would you like to do?',
+           choices: [
+               {
+                   name: 'View All Employees',
+                   value: 'VIEW_EMPLOYEES'
+               }
+           ]
+       } 
+    ])
+};
 
 // Start server after DB connection
 db.connect( (err) => {
     console.log(`Made connection`);
-    init();
 });
-
 
